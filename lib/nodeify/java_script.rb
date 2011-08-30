fdf97686a40f4e0176790d4185c518c63874d38a
Derek Kastner <dkastner@gmail.com>
2011-08-30 08:35:15 -0400

    Return to sandbox for concurrency safety

--- lib/nodeify/java_script.rb
+++ lib/nodeify/java_script.rb
@@ -6,18 +6,17 @@ module Nodeify
   class JavaScript < Sprockets::Processor
     def render(context, options)
       source = ''
-      js_file = File.join('tmp', 'nodeify.js')
-      File.open(js_file, 'w') do |f|
-        f.puts <<-JAVASCRIPT
+      Sandbox.play :path => 'tmp' do |path|
+        js_file = File.join(path, 'nodeify.js')
+        File.open(js_file, 'w') do |f|
+          f.puts <<-JAVASCRIPT
 var browserify = require('browserify');
 var b = browserify({ entry: '#{file}' });
 process.stdout.write(b.bundle());
-        JAVASCRIPT
+          JAVASCRIPT
+        end
+        @source = `node #{js_file}` # TODO: ExecJS failed me here. Also, node can't seem to accept pipes
       end
-      puts "running node #{js_file}"
-      puts "source: #{File.read(js_file)}"
-      @source = `node #{js_file}`
-      FileUtils.rm_f js_file
 
       @source
     end
