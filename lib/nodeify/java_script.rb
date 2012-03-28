f99508367e55324d69357041dcd9ff1a883fdbb5
Derek Kastner <dkastner@gmail.com>
2012-03-27 21:08:50 -0400

    Sandbox kills relative requires

--- lib/nodeify/java_script.rb
+++ lib/nodeify/java_script.rb
@@ -1,17 +1,15 @@
-require 'tilt'
-require 'sandbox'
 require 'sprockets'
+require 'fileutils'
 
 module Nodeify
   class JavaScript < Sprockets::DirectiveProcessor
     def evaluate(context, options, &blk)
       super
 
-      Sandbox.play do |path|
-        file_path = File.join(path, File.basename(file))
-        File.open(file_path, 'w') { |f| f.puts @result }
-        @result = `node -e "var browserify = require('browserify'), _ = process.stdout.write(browserify({ entry: '#{file_path}', require: { http: 'dkastner-http-browserify' } }).bundle());"`
-      end
+      file_path = file + '.tmp'
+      File.open(file_path, 'w') { |f| f.puts @result }
+      @result = `node -e "var browserify = require('browserify'), _ = process.stdout.write(browserify({ entry: '#{file_path}', require: { http: 'http-browserify' } }).bundle());"`
+      FileUtils.rm_f file_path
 
       @result
     end
